ac99633cac5056b1b18642ff9f5422ffbb55c796
Deniz Hashimov <deniz.hashimov@mentormate.com>
2015-01-07 17:23:49 +0200

    new futures

--- MySpringProjects/WorkSpace/DenizTestServer/src/com/deniz/server/DenizHelloWorld.java
+++ MySpringProjects/WorkSpace/DenizTestServer/src/com/deniz/server/DenizHelloWorld.java
@@ -1,9 +1,15 @@
 package com.deniz.server;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import javax.validation.Valid;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -11,9 +17,16 @@ import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
+
+import com.deniz.server.controller.EmpRestURIConstants;
+import com.deniz.server.json.Employee;
 
 @Controller
 public class DenizHelloWorld {
@@ -69,6 +82,75 @@ public class DenizHelloWorld {
 
 		ModelAndView modelAndView = new ModelAndView("AdmissionSuccess");
 		return modelAndView;
+	}
+
+	@RequestMapping(
+			value = "/submitJSON",
+			method = RequestMethod.GET)
+	public MappingJackson2JsonView returnJSON() {
+		return null;
+
+	}
+
+	// Map to store employees, ideally we should use database
+	Map<Integer, Employee> empData = new HashMap<Integer, Employee>();
+
+	@RequestMapping(
+			value = EmpRestURIConstants.DUMMY_EMP,
+			method = RequestMethod.GET)
+	public @ResponseBody
+	Employee getDummyEmployee() {
+		Employee emp = new Employee();
+		emp.setId(9999);
+		emp.setName("Dummy");
+		emp.setCreatedDate(new Date());
+		empData.put(9999, emp);
+		return emp;
+	}
+
+	@RequestMapping(
+			value = EmpRestURIConstants.GET_EMP,
+			method = RequestMethod.GET)
+	public @ResponseBody
+	Employee getEmployee(@PathVariable("id") int empId) {
+
+		return empData.get(empId);
+	}
+
+	@RequestMapping(
+			value = EmpRestURIConstants.GET_ALL_EMP,
+			method = RequestMethod.GET)
+	public @ResponseBody
+	List<Employee> getAllEmployees() {
+		List<Employee> emps = new ArrayList<Employee>();
+		Set<Integer> empIdKeys = empData.keySet();
+		for (Integer i : empIdKeys) {
+			emps.add(empData.get(i));
+		}
+		return emps;
+	}
+
+	@RequestMapping(
+			value = EmpRestURIConstants.CREATE_EMP,
+			method = RequestMethod.POST)
+	public @ResponseBody
+	Employee createEmployee(@Valid @ModelAttribute Employee emp,
+							BindingResult result) {
+		String name = emp.getName();
+
+		emp.setCreatedDate(new Date());
+		empData.put(emp.getId(), emp);
+		return emp;
+	}
+
+	@RequestMapping(
+			value = EmpRestURIConstants.DELETE_EMP,
+			method = RequestMethod.PUT)
+	public @ResponseBody
+	Employee deleteEmployee(@PathVariable("id") int empId) {
+		Employee emp = empData.get(empId);
+		empData.remove(empId);
+		return emp;
 	}
 
 }
---
+++ MySpringProjects/WorkSpace/DenizTestServer/src/com/deniz/server/json/Employee.java
@@ -0,0 +1,45 @@
+package com.deniz.server.json;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.ser.std.DateSerializer;
+
+public class Employee implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 5055087491222734313L;
+
+	private int id;
+	private String name;
+	private Date createdDate;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@JsonSerialize(
+			using = DateSerializer.class)
+	public Date getCreatedDate() {
+		return createdDate;
+	}
+
+	public void setCreatedDate(Date createdDate) {
+		this.createdDate = createdDate;
+	}
+}
